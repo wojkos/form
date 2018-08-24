7840ce66302940e3468f06f71e971557f0247ddb
wojkos <wojtek@tappest.com>
2018-08-24 13:42:20 +0200

    Add rollback

--- app/controllers/formreciver_controller.rb
+++ app/controllers/formreciver_controller.rb
@@ -10,19 +10,21 @@ class FormreciverController < ApplicationController
 
     @user.name = params['name']
     @user.email = params['email']
-    if @user.save
-      @address.user_id = @user.id
-      @address.city = params['city']
-      @address.street_name = params['street_name']
-      @address.street_name_number = params['street_name_number']
-      if @address.save
-        redirect_to users_path, notice: 'Form was successfully created.'
+    ActiveRecord::Base.transaction do
+      if @user.save
+        @address.user_id = @user.id
+        @address.city = params['city']
+        @address.street_name = params['street_name']
+        @address.street_name_number = params['street_name_number']
+        if @address.save
+          redirect_to users_path, notice: 'Form was successfully created.'
+        else
+          render :new
+          raise ActiveRecord::Rollback
+        end
       else
-        @user.destroy!
         render :new
       end
-    else
-      render :new
     end
   end
 end
